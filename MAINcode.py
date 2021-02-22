71caf234c8ac7b2b1c621a52b7fdb56dc1c77c7b
Eli Pacheco <eli.pacheco55@gmail.com>
2021-02-22 10:49:32 -0700

    Added comments

--- MAINcode.py
+++ MAINcode.py
@@ -7,15 +7,15 @@ def explain ():     # Explain what the program is going to do
     print("This program will tell you the area of various objects")
 
 
-def calculate(c):   # Function will calculate the area here
+def calculate(c):   # Function will calculate the area of circle 
     area = round(3.14159*pow(c,2), 3)
     print(f"The are of the circle is: {area}")
 
-def calculate_t(b,h):
+def calculate_t(b,h):  # Function will calculate the are of triangle 
     area = round(0.5*b*h, 3)
     print(f"The area of the triangle is: {area}")
 
-def calculate_s(s): # This function will calculate the area of the sphere 
+def calculate_s(s): # This function will calculate the area and volume of the sphere 
     area = round(3.14159*4*pow(s,2), 3)
     volume = round(3.14159*1.3333333*pow(s,3), 3)
     print(f"The surface area of the sphere is: {area}")
