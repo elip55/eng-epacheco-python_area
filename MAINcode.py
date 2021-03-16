6473902602351f767ee6765c1e0fffb2e223ead7
eli pacheco <elipacheco@Genevas-iMac.local>
2021-03-16 11:46:50 -0600

    Added modules for more advanced code in future

---
+++ MAINcode.py
@@ -0,0 +1,8 @@
+# area with functions
+
+from src.calculations import calculate_c, calculate_s, calculate_t
+
+print("Hello, this is a simple program that will calculate areas of certain objects.")
+calculate_c()
+calculate_s()
+calculate_t()
---
+++ src/variables.py
@@ -0,0 +1,7 @@
+
+
+c = float(input("Input the radius of the circle: "))
+s = float(input("Input the radius of the sphere: "))
+b = float(input("Input the base of the triangle: "))
+h = float(input("Input the height of the triangle: "))
+print("--------------------------------------------")
