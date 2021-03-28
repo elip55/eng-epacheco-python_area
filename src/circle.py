3942a2889d921056ec34f53ff9c8baae82d5b0fa
Eli Pacheco <eli.pacheco55@gmail.com>
2021-03-28 15:40:56 -0600

    Added choices

--- src/circle.py
+++ src/circle.py
@@ -8,11 +8,14 @@ def circle_area():
     area_circle = pi*pow(radius,2)
 
     print(f"The area of the circle is: {area_circle}")
-    print("Now, lets turn the circle into a sphere!")
+    answr = input("Would you like to turn this circle into a sphere and calculate the area/volume? y/n")
     
-    area_sphere = 4*pi*pow(radius, 2)
-    volume_sphere = 1.3333*pi*pow(radius,3)
+    if answr == 'y' or answr == ' y':
+        area_sphere = 4*pi*pow(radius, 2)
+        volume_sphere = 1.3333*pi*pow(radius,3)
 
-    print(f"The surface area of the sphere is: {area_sphere}")
-    print(f"The volume of the sphere is: {volume_sphere}")
+        print(f"The surface area of the sphere is: {area_sphere}")
+        print(f"The volume of the sphere is: {volume_sphere}")
+    else:
+        print("Okay!")
 
